7e29abc198fa14fd19abf198f0d11bfddf8ca3c9
Melvin Obiri <dmouse@dmpro.local>
2023-01-19 16:50:44 +0300

    added product table columns

--- database/migrations/2023_01_19_134719_create_products_table.php
+++ database/migrations/2023_01_19_134719_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->string('image');
+            $table->integer('price');
             $table->timestamps();
         });
     }
