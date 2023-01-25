9bbcfb61657151f8a34a74eb043b862cb2f2ccf8
Melvin Obiri <dmouse@dmpro.local>
2023-01-25 20:01:07 +0300

    refactoring Product Model and AdminProductController

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -18,6 +18,15 @@ class Product extends Model
      * $this->attributes['created_at'] - timestamp - contains the product creation date
      * $this->attributes['updated_at'] - timestamp - contains the product update date
      */
+    public static function validate($request)
+    {
+        $request->validate([
+            "name" => "required|max:255",
+            "description" => "required",
+            "price" => "required|numeric|gt:0",
+            'image' => 'image',
+        ]);
+    }
     public function getId()
     {
         return $this->attributes['id'];
