47a3e86ff47b05ca59a80d32e3f65dad5fc45093
Melvin Obiri <dmouse@dmpro.local>
2023-01-26 14:38:57 +0300

    added changes for purchase on route

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,7 @@ Route::get('/cart/delete', 'App\Http\Controllers\CartController@delete')->name("
 Route::post('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add");
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('/cart/purchase', 'App\Http\Controllers\CartController@purchase')->name("cart.purchase");
-});
+
 
 Route::middleware('admin')->group(function () {
     Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
