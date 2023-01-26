a8df6fe848fda0404cc2d4b4309871f9fcf0e3d7
Melvin Obiri <dmouse@dmpro.local>
2023-01-26 11:34:50 +0300

    added fields for orders table

--- database/migrations/2023_01_26_082916_create_orders_table.php
+++ database/migrations/2023_01_26_082916_create_orders_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->integer('total');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
